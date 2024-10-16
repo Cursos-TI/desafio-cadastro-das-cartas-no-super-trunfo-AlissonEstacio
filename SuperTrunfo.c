5e83fc1870c961943d26c279d39682b64a1b6206
AlissonEstacio <timoteoalisson@gmail.com>
2024-10-16 22:58:26 +0000

    Inserido as primieiras informações

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -4,6 +4,18 @@ int main(){
     printf("Desafio Super Trundo\n");
     printf("Estado Alagoas\n");
 
+    char codigo[4];
+    int populacao;
+    float area;
+    float pib;
+    int num_pontos_turisticos;
+
+    printf("Codigo: %s\n", cidade.codigo);
+    printf("População: %d\n", cidade.populacao);
+    printf("Area: %.2f km²\n", cidade.area);
+    printf("PIB: R$ %.2f\n", cidade.pib);
+    printf("Número de Pontos Turísticos: %d\n", cidade.num_pontos_turisticos);
+
 
 
 
